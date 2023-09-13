eef69973395474df708dd0d7d730f8e0762d706a
A-DDONG <ssc5924@gmail.com>
2023-09-13 16:59:38 +0900

    joystic

--- tab_animal_v3/lib/sprite/constants/asset_constants.dart
+++ tab_animal_v3/lib/sprite/constants/asset_constants.dart
@@ -1,4 +1,4 @@
 class AssetConstants {
-  static String backgroundImage = 'main_ui_background.png';
+  static String backgroundImage = 'main_background.png';
   static String avatarImage = 'cat_sprite.png';
 }
